edc907f59110692a4f078e05a4b45930c699d657
JCASTILLOC2920 <grupopatologia2012@gmail.com>
2026-04-23 03:24:31 -0500

    SUPREME DIRECTIVE: Ultra-Concise Victoria Refactor

--- modulo_chat/config.js
+++ modulo_chat/config.js
@@ -11,11 +11,10 @@ const VICTORIA_CONFIG = {
       knowledgeBase: [
                 "Anatomia Patologica: Especialidad que estudia las causas y efectos de las enfermedades.",
                 "Citologia: Estudio de celulas individuales para deteccion temprana (ej. Papanicolaou).",
-                "Biopsia: Procedimiento para extraer una muestra de tejido para analisis.",
-                "Ubicacion: Mz M2 lote 13 Jardines de Chillon, Puente Piedra, Lima, Peru.",
-                "Telefono / WhatsApp: 986 396 733",
-                "IHQ - Marcadores: CD20 (Linfomas B), CD3 (Linfomas T), Ki-67 (Proliferacion/Agresividad), HER2 (Cancer de Mama/Estomago), ER/PR (Receptores Hormonales), P16 (VPH), TTF-1 (Pulmon/Tiroides).",
-                "IHQ - Costos: 100 soles por marcador sin lectura, 250 soles con lectura/informe.",
-                "Biopsias: Resultados en 3-4 dias habiles. Especialistas en oncologia."
+                "Biopsia: Analisis de tejido.",
+                "Ubicacion: Mz M2 lote 13 Jardines de Chillon, Puente Piedra.",
+                "Telefono/WA: 986396733",
+                "IHQ: 100 soles (un marcador), 250 soles (con informe).",
+                "Biopsias: Entrega en 3-4 dias."
             ]
 };
